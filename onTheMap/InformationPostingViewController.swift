077f25a4401aa6e333e038b099bdea16be993064
npoisonivy <npoisonivy@gmail.com>
2017-03-27 20:06:25 -0700

    layout

--- onTheMap/InformationPostingViewController.swift
+++ onTheMap/InformationPostingViewController.swift
@@ -13,25 +13,44 @@ class InformationPostingViewController: UIViewController {
     @IBOutlet weak var locationInput: UITextField!
     @IBOutlet weak var debugArea: UILabel!
     
+    @IBOutlet weak var locationPostingStackView: UIStackView!
+    @IBOutlet weak var mediaURLPostingStackView: UIStackView!
+    
     @IBAction func cancelBtnPressed(_ sender: AnyObject) {
-        self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil) // go back to mapVC/ StudentsTVC
     }
     
-    @IBAction func findOnMapBtnPressed(_ sender: AnyObject) {
-        // call a func to check if location is empty, and do geocoding
+    @IBAction func findOnMapBtnPressed(_ sender: Any) {
+        // geocode location string -> latitude + longtitude -> pass as var to MKMapView @ POST url view -> at that view, u add Mapview there.
         if locationInput.text!.isEmpty {
-            debugArea.text = "Location cannot be blank."
+            debugArea.text = "Location cannot be blank"
+            // view stays...
         } else {
+            let location = locationInput.text
+            print("location is \(location)")
             // do geocoding, find the map
             
+            // when geocoding is successful, pass geocoding data to a func that does map showingn- save latitiude, longitude as var - easier for retrieving later
             
-            // when geocoding is done, pass geocoding data to a func that does map shoing
             
             
+            // call several calls here
+            // 1. get a student location
+            // if nil call 2. POST a student location
+            // if != nil, 3. PUT a student location
+            
+            
+            // after saving var is done, bring user to the view 2.
+            locationPostingStackView.isHidden = true
+            mediaURLPostingStackView.isHidden = false
+            // UIView.animateWithDuration - to add fading effect
+            
+            // no need for below - because both views share the SAME view controller....
             //  when data is passed, bring to next controller.
-            let controller = self.storyboard!.instantiateViewController(withIdentifier: "MediaPostingViewController") 
-            self.present(controller, animated: true, completion: nil)
-        
+            // let controller = self.storyboard!.instantiateViewController(withIdentifier: "MediaPostingViewController")
+            // self.present(controller, animated: true, completion: nil)
+
+            
         }
         
     }
@@ -40,11 +59,17 @@ class InformationPostingViewController: UIViewController {
         super.viewDidLoad()
         
         debugArea.text = ""
+        locationPostingStackView.isHidden = false
+        mediaURLPostingStackView.isHidden = true
         
-        
-        
+        // just for testing - delete after !
+        //locationPostingStackView.isHidden = true
+        //mediaURLPostingStackView.isHidden = false
         
         
     }
+ 
+    
+
 
 }
